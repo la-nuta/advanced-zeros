933039a4c4bf7240f310d0bfe46bd96e398708d7
Anna Marfel <anna.marfel@gmail.com>
2018-03-06 00:29:37 +0300

    Task is done refactor: split out to different functions

--- src/index.js
+++ src/index.js
@@ -1,51 +1,64 @@
 module.exports = function getZerosCount(number, base) {
 
-    let arrSimpleNum = [];
-    let arrCountSimpleNum = [];
+    let allSimpleDividers = findSimpleDividers();
 
-    for (let i = 2; i <= base; i++) {
-        if (!(base % i)) {
+    return countZeros(allSimpleDividers);
+
+    function findSimpleDividers() {
+        let arrSimpleNum = [];
+        let arrCountSimpleNum = [];
+
+        for (let i = 2; i <= base; i++) {
+            if (!(base % i)) {
+                base = Math.floor(base / i);
+                let check = 1;
+
+                for (let j = 2; j < i; j++) {
+                    if (!(i % j) && j !== i) {
+                        check = 0;
+                    }
+                }
+
+                collectAllSimpleDividers(check, i, arrSimpleNum, arrCountSimpleNum);
+            }
+        }
+        return {arrSimpleNum, arrCountSimpleNum};
+    }
+
+    function collectAllSimpleDividers(check, i, arrSimpleNum, arrCountSimpleNum) {
+        if (check === 1) {
+            arrSimpleNum.push(i);
+            countSimpleDividers(i, arrSimpleNum, arrCountSimpleNum);
+        }
+    }
+
+    function countSimpleDividers(i, arrSimpleNum, arrCountSimpleNum) {
+        let count = 1;
+        arrCountSimpleNum.push(count);
+
+        while (!(base % i)) {
             base = Math.floor(base / i);
-            let check = 1;
-
-            for (let j = 2; j < i; j++) {
-                if (!(i % j) && j !== i) {
-                    check = 0;
-                }
-            }
-            if (check === 1) {
-                arrSimpleNum.push(i);
-                let count = 1;
-                arrCountSimpleNum.push(count);
-
-                while (!(base % i)) {
-                    base = Math.floor(base / i);
-                    arrSimpleNum.push(i);
-                    count = count + 1;
-                    arrCountSimpleNum.push(count);
-                }
-            }
+            arrSimpleNum.push(i);
+            count = count + 1;
+            arrCountSimpleNum.push(count);
         }
     }
 
-    let countArr = [];
+    function countZeros({arrSimpleNum, arrCountSimpleNum}) {
+        let countArr = [];
+        for (let i = 0; i < arrSimpleNum.length; i++) {
+            let countNum = number;
+            let count = 0;
 
-
-    for (let i = 0; i < arrSimpleNum.length; i++) {
-        let countNum = number;
-        let count = 0;
-
-        while (countNum > 0) {
-            countNum = Math.floor(countNum / arrSimpleNum[i]);
-            count += countNum;
-
+            while (countNum > 0) {
+                countNum = Math.floor(countNum / arrSimpleNum[i]);
+                count += countNum;
+                }
+            if (arrSimpleNum[i] === arrSimpleNum[i - 1]) {
+                count = Math.floor(count / arrCountSimpleNum[i]);
+            }
+            countArr.push(count);
         }
-        if (arrSimpleNum[i] === arrSimpleNum[i - 1]) {
-            count = Math.floor(count / arrCountSimpleNum[i]);
-        }
-        countArr.push(count);
+        return Math.min.apply(null, countArr);
     }
-
-    return Math.min.apply(null, countArr);
 }
-
